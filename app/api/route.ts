a62f9c48c25fbcfb40eebe83c025001b0a60c5cf
Jasurbek1810 <JasurbekSaydullayev@gmail.com>
2024-03-25 14:36:02 +0500

    Initial commit from Create Next App

---
+++ app/api/route.ts
@@ -0,0 +1,15 @@
+export async function GET(request: Reaquest){
+    return Response.json("GET request sent")
+}
+export async function POST(request: Reaquest){
+    return Response.json("POST request sent")
+}
+export async function PUT(request: Reaquest){
+    return Response.json("GET request sent")
+}
+export async function GET(request: Reaquest){
+    return Response.json("GET request sent")
+}
+export async function GET(request: Reaquest){
+    return Response.json("GET request sent")
+}
